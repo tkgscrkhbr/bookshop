5e663adc7e77370b55261b8c786461febefbb511
Elad Berliner <eladberliner@gmail.com>
2024-06-02 15:40:17 +0300

    oninit quick

---
+++ js/book.controller.js
@@ -0,0 +1,25 @@
+'use strict'
+
+function oninit(){
+    render()
+}
+
+function render(){
+var elTbody= document.querySelector('tbody')
+// var books=getBooks()
+// console.log(books)
+var str= getBooks().map(book => 
+    `<tr>
+    <td>${book.title}</td>
+    <td>${book.price}</td>
+    <td>
+        <button>read</button>
+        <button>update</button>
+        <button>delete</button>
+    </td>
+
+</tr>`)
+// console.log(str)
+elTbody.innerHTML=str.join('')
+// console.log(elTbody.innerHTML)
+}
---
+++ js/services/book.service.js
@@ -0,0 +1,21 @@
+'use strict'
+
+const gBooks=[
+{ id:'t101',
+title:'harry potter',
+price:120,
+},
+{ id:'t102',
+title:'the adventures of',
+price:120,
+},
+{ id:'t103',
+title:'Shrek',
+price:120,
+}
+
+]
+
+function getBooks(){
+    return gBooks
+}
